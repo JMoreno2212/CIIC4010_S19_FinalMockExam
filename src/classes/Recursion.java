09fb864d391ef8789429bc14ee968f920f4cd920
yumim <yumim@DESKTOP-6722K1G>
2019-05-16 13:53:48 -0400

    Recursion part DONE!

---
+++ src/classes/Recursion.java
@@ -0,0 +1,51 @@
+package classes;
+
+
+public class Recursion {
+   
+    /**
+     * Exercise #1: In this exercise you must find the sum of all the digits within a number. 
+     * Example: If your number is 123 then the sum of the digits is 1 + 2 + 3 = 6.
+     * NOTE: It must be solved recursively.
+     * @param num - Number that is going to add.
+     * @return Returns the sum of the digits of the number. 
+     */
+    public static int sumOfDigits(int num){
+    	
+    	// Add your code here.
+    	
+    	return 0;//Dummy Return
+                           
+    }
+    /**
+     * Exercise #2: In this exercise you must find the sum of all the elements of an array of integers.
+     * NOTE: It must be solved recursively. You need to implement a helper function.
+     * @param arr - The array of integers.
+     * @return Returns the sum of all the elements of the array.
+     */
+    public static int sumOfElements(int[] arr){
+    	
+    	// Add your code here.
+    	
+    	return 0;//Dummy Return
+     
+    }
+    
+    /**
+     * Exercise #3: In this exercise you must return the given word but the other way around.
+     * Example: If the word is "Recursion" it should return "noisruceR".
+     * NOTE: It must be solved recursively.
+     * @param word - The word that is going to invert.
+     * @param wordLength - The length of the word.
+     * @return Returns the word backwards.
+     */
+    public static String invertWord(String word, int wordLength){
+    	
+    	// Add your code here.
+    	
+    	return null;//Dummy Return
+  
+    }
+ 
+}
+
